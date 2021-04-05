6603b90825eac688d790cc10e9974f697401f18d
nindium <nindium@gmail.com>
2021-04-05 09:01:31 -0400

    minor fixes

--- eks.tf
+++ eks.tf
@@ -82,7 +82,7 @@ resource "aws_eks_node_group" "wnodes" {
   }
 
   # It needs to make sure that Role permissions are created before and deleted after K8S Node Group handling.
-  # Otherway cluster could not be properlyt destroyed. 
+  # Otherway cluster could not be properly destroyed. 
   depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
